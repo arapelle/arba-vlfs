ea55953ed4cfee424925b19089b46c381ab1d44a
Aymeric Pellé <aympelle@gmail.com>
2020-09-22 17:16:56 +0200

    v0.1.0:VLFS#1: Update examples.

--- example/basic_cmake_project/main.cpp
+++ example/basic_cmake_project/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <vlfs/vlfs.hpp>
 
+using namespace strn::literals;
+
 int main()
 {
+    vlfs::virtual_filesystem vfs;
+    vfs.set_virtual_root("VROOT"_s64,  "/tmp");
+    vfs.set_virtual_root("RSC"_s64,    "VROOT:/rsc");
+    vfs.set_virtual_root("IMAGES"_s64, "RSC:/images");
+
+    std::filesystem::path path("IMAGES:/file");
+    vfs.convert_to_real_path(path);
+    std::cout << path << std::endl;
+
     return EXIT_SUCCESS;
 }
